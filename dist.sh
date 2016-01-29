aa222482814e4cea4f2c55b8ded630c643f67d6f
yao hai tao <addday@gmail.com>
2016-01-29 16:38:49 +0800

    to linux conf fix

--- dist.sh
+++ dist.sh
@@ -4,7 +4,7 @@ mkdir -p dist/$1/provider
 cp -rf provider/config dist/$1/provider/config
 cp -rf provider/config-$1 dist/$1/provider/config
 cp -rf provider/run*.sh dist/$1/provider
-cp provider/target/cardone-*-provider-*.jar dist/$1/provider
+cp provider/target/cardone-cms-*.jar dist/$1/provider
 mkdir -p dist/$1/consumer
 cp -rf consumer/webapp dist/$1/consumer/webapp
 cp -rf consumer/views dist/$1/consumer/views
