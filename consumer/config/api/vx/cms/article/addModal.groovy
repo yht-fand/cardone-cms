5bfa6d9e3811d77434968c0e8d49fd124f92a71e
addday <addday@gmail.com>
2017-06-14 15:36:49 +0800

    fix:支持缓存

--- consumer/config/api/vx/cms/article/addModal.groovy
+++ consumer/config/api/vx/cms/article/addModal.groovy
@@ -5,7 +5,7 @@ import top.cardone.cms.service.ArticleService
 import top.cardone.context.ApplicationContextHolder
 import top.cardone.core.CodeException
 
-class addModal {
+class addModal implements java.io.Serializable {
     def input(input) {
         input
     }
