ee5f45d39300a9732659bdab3e286a6fc027eee5
addday <addday@gmail.com>
2017-07-17 11:36:03 +0800

    优化性能

--- api/src/main/java/top/cardone/cms/service/ArticleService.java
+++ api/src/main/java/top/cardone/cms/service/ArticleService.java
@@ -22,8 +22,8 @@ import java.util.Map;
  */
 @Transactional(readOnly = true)
 @CacheConfig(cacheNames = {"top.cardone.cms.service.ArticleService"})
-@Events({@Event(applicationEvent = SimpleErrorEvent.class, configs = {"insertOperateLogAction"}),
-        @Event(applicationEvent = SimpleEvent.class, configs = {"insertOperateLogAction"})})
+@Events({@Event(applicationEvent = SimpleErrorEvent.class),
+        @Event(applicationEvent = SimpleEvent.class)})
 public interface ArticleService extends PageService {
     /**
      * @see top.cardone.cms.service.ArticleService#page
--- api/src/main/java/top/cardone/cms/service/NoticeService.java
+++ api/src/main/java/top/cardone/cms/service/NoticeService.java
@@ -22,8 +22,8 @@ import java.util.Map;
  */
 @Transactional(readOnly = true)
 @CacheConfig(cacheNames = {"top.cardone.cms.service.NoticeService"})
-@Events({@Event(applicationEvent = SimpleErrorEvent.class, configs = {"insertOperateLogAction"}),
-        @Event(applicationEvent = SimpleEvent.class, configs = {"insertOperateLogAction"})})
+@Events({@Event(applicationEvent = SimpleErrorEvent.class),
+        @Event(applicationEvent = SimpleEvent.class)})
 public interface NoticeService extends PageService {
     /**
      * @see top.cardone.cms.service.NoticeService#page
