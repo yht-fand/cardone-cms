51e82c378c2cefc66d77acb36629937f6db1f07a
addday@gmail.com <addday@gmail.com>
2018-06-25 11:18:18 +0800

    升级到 spring 5.0 及 spring boot 2.0

--- consumer/src/main/java/top/cardone/ConsumerApplication.java
+++ consumer/src/main/java/top/cardone/ConsumerApplication.java
@@ -1,7 +1,6 @@
 package top.cardone;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -23,7 +22,6 @@ import top.cardone.context.annotation.CardOneAnnotationBeanNameGenerator;
         org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration.class
 })
 @ImportResource({"file:config/applicationContext.xml", "file:config/applicationContext.groovy"})
-@EnableAutoConfiguration
 public class ConsumerApplication {
     /**
      * 主方法
